d468b2ef0a3a51476895118386a4a966d9a43339
skynetfrank <frank.uah@gmail.com>
2025-01-06 19:01:56 -0400

    lesson 12 cart page

--- hooks/use-cart-store.ts
+++ hooks/use-cart-store.ts
@@ -17,6 +17,8 @@ const initialState: Cart = {
 interface CartState {
   cart: Cart;
   addItem: (item: OrderItem, quantity: number) => Promise<string>;
+  updateItem: (item: OrderItem, quantity: number) => Promise<void>;
+  removeItem: (item: OrderItem) => void;
 }
 
 const useCartStore = create(
@@ -61,6 +63,40 @@ const useCartStore = create(
         return updatedCartItems.find(
           (x) => x.product === item.product && x.color === item.color && x.size === item.size
         )?.clientId!;
+      },
+      updateItem: async (item: OrderItem, quantity: number) => {
+        const { items } = get().cart;
+        const exist = items.find((x) => x.product === item.product && x.color === item.color && x.size === item.size);
+        if (!exist) return;
+        const updatedCartItems = items.map((x) =>
+          x.product === item.product && x.color === item.color && x.size === item.size
+            ? { ...exist, quantity: quantity }
+            : x
+        );
+        set({
+          cart: {
+            ...get().cart,
+            items: updatedCartItems,
+            ...(await calcDeliveryDateAndPrice({
+              items: updatedCartItems,
+            })),
+          },
+        });
+      },
+      removeItem: async (item: OrderItem) => {
+        const { items } = get().cart;
+        const updatedCartItems = items.filter(
+          (x) => x.product !== item.product || x.color !== item.color || x.size !== item.size
+        );
+        set({
+          cart: {
+            ...get().cart,
+            items: updatedCartItems,
+            ...(await calcDeliveryDateAndPrice({
+              items: updatedCartItems,
+            })),
+          },
+        });
       },
       init: () => set({ cart: initialState }),
     }),
