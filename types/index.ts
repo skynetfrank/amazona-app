d553d35cd214fde359f090925f8c865777163c13
skynetfrank <frank.uah@gmail.com>
2025-01-07 07:58:56 -0400

    signin not done home

--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,10 @@
-import { ProductInputSchema, CartSchema, OrderItemSchema } from "@/lib/validator";
+import { ProductInputSchema, CartSchema, OrderItemSchema, UserInputSchema, UserSignInSchema } from "@/lib/validator";
 import { z } from "zod";
 
 export type IProductInput = z.infer<typeof ProductInputSchema>;
 
 export type Data = {
+  users: IUserInput[];
   products: IProductInput[];
   headerMenus: {
     name: string;
@@ -19,3 +20,6 @@ export type Data = {
 };
 export type OrderItem = z.infer<typeof OrderItemSchema>;
 export type Cart = z.infer<typeof CartSchema>;
+
+export type IUserInput = z.infer<typeof UserInputSchema>;
+export type IUserSignIn = z.infer<typeof UserSignInSchema>;
